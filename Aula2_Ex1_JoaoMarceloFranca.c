c56ba126d99f389dc57bbc88f6ab6c1cb9a3a0ed
João Marcelo França Batista <130785029+JmFranca21@users.noreply.github.com>
2023-07-25 19:27:31 -0300

    Update Aula2_Ex1_JoaoMarceloFranca.c

--- Aula2_Ex1_JoaoMarceloFranca.c
+++ Aula2_Ex1_JoaoMarceloFranca.c
@@ -1,15 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+float RecebeCotacao () {
+    float cotacao;
+    printf("Digite a cotacao do dolar: ");
+    scanf("%f", &cotacao);
+    return cotacao;
+}
+
+float RecebeValor () {
+    float valor;
+    printf("Digite o valor em dolar: ");
+    scanf("%f", &valor);
+    return valor;
+}
+
+float ConverteEmReal () {
+    float cotacao = RecebeCotacao();
+    float valor = RecebeValor();
+    float real = cotacao * valor;
+    return real;
+}
 int main()
 {
-    
-    float c;
-    float d;
-    printf("Insira a cotação do dólar hoje: ");
-    scanf("%f", &c);
-    printf("Insira a quantia de dinheiro que você tem em dólar: ");
-    scanf("%f", &d);
-    float r = c*d;
-    printf("A quantia informada em dólares equivale a %.2f reais, de acordo com a cotação de hoje.", r);
-    
+    float real = ConverteEmReal();
+    system("cls");
+    printf("O valor em reais eh: RS %.2f", real);
+    return 0;
 }
